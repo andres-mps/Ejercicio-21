52c4733f8259e4258b9907d7647419a8947b68a2
Pataty22 <110777470+Pataty22@users.noreply.github.com>
2023-05-04 15:12:37 -0300

    second-change

--- controllers/mainControllers.js
+++ controllers/mainControllers.js
@@ -1,18 +1,41 @@
 const { Sequelize, Model, DataTypes } = require("sequelize");
 
-/*const sequelize = new Sequelize(
-    "test",
+const sequelize = new Sequelize(
+    "ejercicio_21",
     "root",
     "50137667",
     {
         host: "localhost",
         port: 3306,
         dialect: "mysql",
-});*/
+});
 
+async function viewAdmin(req,res){
+    res.redirect("/admin")
+};
+
+/*class User extends Model {};
+User.init({
+        id:{
+            type: DataTypes.BIGINT.UNSIGNED,  //sequelize otortga diccionario- tipo de dato- tiene que ser positivo, por eso el .unsigned
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        firstname : {
+            type: DataTypes.STRING(100),
+            allowNull: false,
+        },
+        lastname: {
+            type: DataTypes.STRING(100),
+            allowNull: false,
+        }
+    },
+{ sequelize, modelName: "user",timestamps:false }
+);*/
 
 
 
 module.exports ={
+    viewAdmin,
 
 };
--- routes/routes.js
+++ routes/routes.js
@@ -2,8 +2,17 @@ const express = require("express");
 const routes = express.Router();
 const mainController = require("../controllers/mainControllers.js");
 
+<<<<<<< Updated upstream
 routes.get("/", (req, res) => {
   res.render("home");
 });
 
 module.exports = routes;
+=======
+routes.get("/admin", mainController.viewAdmin);
+
+
+
+
+module.exports= routes;
+>>>>>>> Stashed changes
