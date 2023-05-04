8bfa7731609fdd60b02c598f934f72b4023e32ac
Pataty22 <110777470+Pataty22@users.noreply.github.com>
2023-05-04 15:15:22 -0300

    viewAdmin

--- routes/routes.js
+++ routes/routes.js
@@ -2,17 +2,10 @@ const express = require("express");
 const routes = express.Router();
 const mainController = require("../controllers/mainControllers.js");
 
-<<<<<<< Updated upstream
 routes.get("/", (req, res) => {
   res.render("home");
 });
 
-module.exports = routes;
-=======
 routes.get("/admin", mainController.viewAdmin);
 
-
-
-
-module.exports= routes;
->>>>>>> Stashed changes
+module.exports = routes;
