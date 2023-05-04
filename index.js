cd220847909441d6453dbeecb98f5b589a88ae5d
valentinalr <valentina.lerose@outlook.com>
2023-05-04 14:26:26 -0300

    Footer
    footer

--- index.js
+++ index.js
@@ -3,11 +3,8 @@ const app = express();
 const port = 3000;
 const routes = require("./routes/routes.js");
 
-
-
 app.set("view engine", "ejs");
-
+app.use(express.static("public"));
 app.use(routes);
 
-
-app.listen(port, ()=> console.log(`http://localhost:${port}`));
+app.listen(port, () => console.log(`http://localhost:${port}`));
