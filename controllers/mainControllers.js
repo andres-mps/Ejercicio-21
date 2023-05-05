227181698827acb0800e9a0cd3a276d278ebff19
valentinalr <valentina.lerose@outlook.com>
2023-05-05 16:15:12 -0300

    update_vale_05_05

--- controllers/mainControllers.js
+++ controllers/mainControllers.js
@@ -1,100 +1,97 @@
 require("dotenv").config();
 const { Sequelize, Model, DataTypes } = require("sequelize");
 
-
 const sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USERNAME,
-    process.env.DB_PASSWORD,
-    {
-      host: process.env.DB_HOST,
-      port: process.env.DB_PORT,
-      dialect: process.env.DB_DIALECT,
-    }
-  );
-
-
-class Article extends Model {};
-Article.init({
-        id:{
-            type: DataTypes.BIGINT.UNSIGNED,  
-            primaryKey: true,
-            autoIncrement: true,
-        },
-        title : {
-            type: DataTypes.STRING(100),
-            allowNull: false,
-        },
-        content: {
-            type: DataTypes.STRING(1500),
-            allowNull: false,
-        },
-        author:{
-            type: DataTypes.STRING(100),
-            allowNull: false
-        },
-
-    },
-{ sequelize, modelName: "articles"}
+  process.env.DB_NAME,
+  process.env.DB_USERNAME,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: process.env.DB_DIALECT,
+  }
 );
 
-sequelize.sync({alert:true})
-
-class Author extends Model {};
-Author.init({
-        id:{
-            type: DataTypes.BIGINT.UNSIGNED,  
-            primaryKey: true,
-            autoIncrement: true,
-        },
-        firstname : {
-            type: DataTypes.STRING(100),
-            allowNull: false,
-        },
-        lastname: {
-            type: DataTypes.STRING(1500),
-            allowNull: false,
-        },
-        email:{
-            type: DataTypes.STRING(100),
-            allowNull: false
-        },
-
+class Article extends Model {}
+Article.init(
+  {
+    id: {
+      type: DataTypes.BIGINT.UNSIGNED,
+      primaryKey: true,
+      autoIncrement: true,
     },
-{ sequelize, modelName: "authors"}
+    title: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+    },
+    content: {
+      type: DataTypes.STRING(1500),
+      allowNull: false,
+    },
+    author: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+    },
+  },
+  { sequelize, modelName: "articles" }
 );
 
-sequelize.sync({alert:true})
-
-class Coment extends Model {};
-Coment.init({
-        id:{
-            type: DataTypes.BIGINT.UNSIGNED,  
-            primaryKey: true,
-            autoIncrement: true,
-        },
-        content: {
-            type: DataTypes.STRING(800),
-            allowNull: false,
-        },
-
+class Author extends Model {}
+Author.init(
+  {
+    id: {
+      type: DataTypes.BIGINT.UNSIGNED,
+      primaryKey: true,
+      autoIncrement: true,
     },
-{ sequelize, modelName: "coments", timestamps:false}
+    firstname: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+    },
+    lastname: {
+      type: DataTypes.STRING(1500),
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+    },
+  },
+  { sequelize, modelName: "authors" }
 );
 
-sequelize.sync({alert:true})
+class Coment extends Model {}
+Coment.init(
+  {
+    id: {
+      type: DataTypes.BIGINT.UNSIGNED,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    content: {
+      type: DataTypes.STRING(800),
+      allowNull: false,
+    },
+  },
+  { sequelize, modelName: "coments", timestamps: false }
+);
 
-async function viewAdmin(req,res){
-    
-    res.render("admin");
-};
+sequelize.sync({ alert: true });
 
-async function viewArticle(req,res){
-    res.render("article");
-};
+async function viewHome(req, res) {
+  res.render("home");
+}
 
-module.exports ={
-    viewAdmin,
-    viewArticle
+async function viewAdmin(req, res) {
+  res.render("admin");
+}
 
+async function viewArticle(req, res) {
+  res.render("article");
+}
+
+module.exports = {
+  viewHome,
+  viewAdmin,
+  viewArticle,
 };
