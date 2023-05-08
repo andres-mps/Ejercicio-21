d2fe414ccd7a0eba48db017e06479055c947b5ed
Ivanna Sugliano <126933494+Noah1711@users.noreply.github.com>
2023-05-08 15:17:07 -0300

    addComment

--- controllers/articleController.js
+++ controllers/articleController.js
@@ -1,5 +1,6 @@
 //en teoría esta función es para que se vean los comentarios en el artículo
-//ordenados por fecha de creación- Paty
+//ordenados por fecha de creación
+const connection = require("../models/main")
 
 async function viewArticle(req, res) {
   const articleId = req.params.id;
@@ -9,20 +10,18 @@ async function viewArticle(req, res) {
   });
   res.render("article", { comments });
 }
-//----------------------------------------------------------
+
 async function addComment(req, res) {
-  try {
-    const { name, comment } = req.body;
-    const newComment = await Comment.create({
-      name,
-      comment,
-      article: req.params.id,
-    });
+  try{
+    const { name,content } = req.body;
+    const result = (await connection).execute("INSERT INTO comments (name, content) VALUES (?, ?)", [name, content]);
     res.redirect("/article");
-  } catch (error) {
+} catch (error) {
     console.error(error);
-  }
+    res.status(500).send("Error creating comment");
+};
 }
+
 
 module.exports = {
   viewArticle,
