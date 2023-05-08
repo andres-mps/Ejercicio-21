27877fa8c4fc524c04036184e25163abc5a79e4b
valentinalr <valentina.lerose@outlook.com>
2023-05-08 14:01:29 -0300

    seeder comments

--- seeders/runAllSeeders.js
+++ seeders/runAllSeeders.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 async function runAllSeeders() {
   await require("./userSeeder")();
   await require("./articleSeeder")();
+  await require("./commentSeeder")();
 }
 
 runAllSeeders();
