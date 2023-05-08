3225e222f2416e75cdffb15d4fd63f9fb1a08c75
Pataty22 <110777470+Pataty22@users.noreply.github.com>
2023-05-08 11:44:24 -0300

    .

--- controllers/articleController.js
+++ controllers/articleController.js
@@ -1,5 +1,5 @@
 //en teoría esta función es para que se vean los comentarios en el artículo
-//ordenados por fecha de creación
+//ordenados por fecha de creación- Paty
 
 async function viewArticle(req, res) {
   const articleId = req.params.id;
@@ -9,21 +9,20 @@ async function viewArticle(req, res) {
   });
   res.render("article", { comments });
 }
-
+//----------------------------------------------------------
 async function addComment(req, res) {
   try {
     const { name, comment } = req.body;
-     const newComment = await Comment.create({
+    const newComment = await Comment.create({
       name,
       comment,
       article: req.params.id,
-     });
-     res.redirect("/article");
-    }catch (error) {
-      console.error(error);
-     }
+    });
+    res.redirect("/article");
+  } catch (error) {
+    console.error(error);
   }
-
+}
 
 module.exports = {
   viewArticle,
