41d9d1ed835bfcb4f79590b5f8d90bfd2800d666
andres-mps <andresmps@gmail.com>
2023-05-11 16:26:45 -0300

    andres

--- index.js
+++ index.js
@@ -1,39 +1,37 @@
-const models = require("./models");
 require("dotenv").config();
-const { User } = require("./models");
+
 const express = require("express");
 const app = express();
 const APP_PORT = process.env.APP_PORT || 3000;
 const routes = require("./routes/routes.js");
 const session = require("express-session");
 const { passport, passportConfig } = require("./config/passport");
-const bcrypt = require("bcryptjs");
+
 const makeUserAvailableInViews = require("./middleware/makeUserAvailableInViews");
 const flash = require("connect-flash");
 const mwFlash = require("./middleware/flash");
+const isAuthenticated = require("./middleware/isAuthenticated");
 
 app.use(
   session({
     secret: "milanesa",
     resave: false,
     saveUninitialized: false,
-  })
+  }),
 );
 app.use(flash());
 
 app.use(passport.session());
 passportConfig();
-app.use(makeUserAvailableInViews);
+app.use(makeUserAvailableInViews); // disponibiliza info de req.user a res.local.user
 
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
-app.use(mwFlash);
-app.use(function (req, res, next) {
-  res.locals.login = req.isAuthenticated();
-  next();
-});
+app.use(mwFlash); // para mostrar mensajes de FLASH en login
+app.use(isAuthenticated); // para chequear si usuario está logueado y disponibilizar conetnido.
 app.use(routes);
+
 app.listen(APP_PORT, () => {
   console.log(`\n[Express] Servidor corriendo en el puerto ${APP_PORT}.`);
   console.log(`[Express] Ingresar a http://localhost:${APP_PORT}.\n`);
